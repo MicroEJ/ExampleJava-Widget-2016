83bd84357502467cee6a02ca19a92bc434677c72
privron <privron@is2t-intern.com>
2016-06-29 10:57:28 +0200

    WI16624 Add Sandboxed Nature

---
+++ ej.demo.ui.widget/src/main/java/ej/demo/ui/widget/MainActivity.java
@@ -0,0 +1,64 @@
+/*
+ * Java
+ *
+ * Copyright 2016 IS2T. All rights reserved.
+ * IS2T PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package ej.demo.ui.widget;
+
+import ej.wadapps.app.Activity;
+
+/**
+ *
+ */
+public class MainActivity implements Activity {
+
+	@Override
+	public String getID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRestart() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStart() {
+		WidgetsDemo.main(new String[0]);
+
+	}
+
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
